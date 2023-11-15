153ae78d15a152bbb0f8427ab7c7f8612d7cc7e1
Romu10 <romulobryanp@gmail.com>
2023-11-15 23:03:13 +0000

    Navigation working GOOD

---
+++ path_planner_server/launch/pathplanner.launch.py
@@ -0,0 +1,83 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', '/home/user/ros2_ws/src/warehouse_project/path_planner_server/rviz/pathplanning.rviz'],
+            parameters=[{'use_sim_time': True}]
+        ),
+        DeclareLaunchArgument(
+            'map_file',
+            default_value='/home/user/ros2_ws/src/warehouse_project/map_server/config/warehouse_map_sim.yaml',
+            description='Full path to the map file.'
+        ),
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[
+                {'use_sim_time': True},
+                {'yaml_filename': LaunchConfiguration('map_file')}
+            ]
+        ),
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=["/home/user/ros2_ws/src/warehouse_project/localization_server/config/amcl_config.yaml"]
+        ),
+        Node(
+            package='nav2_planner',
+            executable='planner_server',
+            name='planner_server',
+            output='screen',
+            parameters=["/home/user/ros2_ws/src/warehouse_project/path_planner_server/config/planner_server.yaml"]
+        ),
+    
+        Node(
+            package='nav2_controller',
+            executable='controller_server',
+            output='screen',
+            remappings=[('/cmd_vel', '/robot/cmd_vel')],
+            parameters=["/home/user/ros2_ws/src/warehouse_project/path_planner_server/config/controller.yaml"]
+        ),
+        
+        Node(
+             package='nav2_bt_navigator',
+            executable='bt_navigator',
+            name='bt_navigator',
+            output='screen',
+            parameters=["/home/user/ros2_ws/src/warehouse_project/path_planner_server/config/bt.yaml"]
+        ),
+
+        Node(
+            package='nav2_recoveries',
+            executable='recoveries_server',
+            name='recoveries_server',
+            parameters=["/home/user/ros2_ws/src/warehouse_project/path_planner_server/config/recovery.yaml"],
+            output='screen'
+        ),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager',
+            output='screen',
+            parameters=[{'use_sim_time': True},
+                        {'autostart': True},
+                        {'node_names': ['map_server','amcl','planner_server', 'controller_server', 'bt_navigator', 'recoveries_server']}],
+        ),
+
+    ])
